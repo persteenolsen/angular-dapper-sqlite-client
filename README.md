d459a9033d4189926d721c34aae28382460ca83a
Per Olsen <persteen22@gmail.com>
2024-02-24 18:00:44 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-14-crud-example
+
+Angular 14 - CRUD Example with Reactive Forms
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://dotnet.dapper.sqlite.api.persteenolsen.com'
+};
